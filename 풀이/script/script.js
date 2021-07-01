6bfb0cd25181e7d6c1478ddda9e4cdb3c6ce2354
arenneu <jisusjy@naver.com>
2021-07-01 14:20:38 +0900

    0701_juSoKr

--- 풀이/script/script.js
+++ 풀이/script/script.js
@@ -2,14 +2,38 @@
 
 $(function(){
 
+    /* GNB메뉴 시작 */
     $("li.mainLi").mouseover(function(){
-        $("ul.subMenu").show();
-        $("div#gnbBGFrame").show();
+        $("ul.subMenu").stop().show();
+        $("div#gnbBGFrame").stop().show();
     });
 
     $("li.mainLi").mouseout(function(){
         $("ul.subMenu").hide();
         $("div#gnbBGFrame").hide();
     });
+    /* GNB메뉴 끝 */
+
+    /* 주소검색 시작 */
+    $("input#searchWord").keyup(function(){
+        var txt = $("input#searchWord").val(); // 변수:값을 임시저장
+
+        // txt에 저장된 값이 비어있는 공백(="")이 아니라면
+        // 제이쿼리의 css를 사용하여 x마크를 표시하세요
+
+        if (txt == "") {  // 텍스트박스의 값이 공백이라면 x마크를 숨김
+            $("span#closeMark").css({"visibility":"hidden"});
+        }
+
+        if (txt != "") { // 텍스타박스이 값이 공백이 아니라면 x마크를 표시
+            $("span#closeMark").css({"visibility":"visible"});
+        }
+
+
+
+
+
+    });
+    /* 주소검색 끝 */
 
 });
